330ef478f8e5f3eca2cc6ecd21ba2febd23e5780
Ankush Maity <maityankush507@gmail.com>
2026-02-22 20:34:31 +0530

    Refactor sales_service.py for model loading path
    Added base directory path for model loading and included debug prints for model path and existence check.

--- backend/app/services/sales_service.py
+++ backend/app/services/sales_service.py
@@ -2,13 +2,17 @@ import joblib
 import os
 import pandas as pd
 
-MODEL_PATH = os.path.join("models_saved", "sales_prediction_model.pkl")
+# Get base directory of backend
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+MODEL_PATH = os.path.join(BASE_DIR, "models_saved", "sales_prediction_model.pkl")
+
+print("Loading model from:", MODEL_PATH)
+print("File exists:", os.path.exists(MODEL_PATH))
 
 model = joblib.load(MODEL_PATH)
 
 def predict_sales(input_data: dict):
-
     df = pd.DataFrame([input_data])
     prediction = model.predict(df)
-
     return float(prediction[0])
